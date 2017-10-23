49c2bfd629c7da358348f8df7bd45feeb068c224
Helgi Þorbjörnsson <helgith@gmail.com>
2017-10-23 16:09:58 -0700

    Move user-data to a simple template file

--- scripts/terraform/instance.tf
+++ scripts/terraform/instance.tf
@@ -1,3 +1,7 @@
+data "template_file" "user_data" {
+  template = "${file("user-data.tpl")}"
+}
+
 resource "aws_instance" "node" {
   count                       = "${var.node_count}"
   ami                         = "${data.aws_ami.base.id}"
@@ -9,30 +13,11 @@ resource "aws_instance" "node" {
   subnet_id                   = "${element(aws_subnet.private.*.id, count.index % length(aws_subnet.private.*.id))}"
   iam_instance_profile        = "${aws_iam_instance_profile.node.id}"
   ebs_optimized               = true
+  user_data                   = "${data.template_file.user_data.rendered}"
+
   lifecycle {
      ignore_changes = [ "user_data", "instance_type" ]
   }
-
-  user_data = <<EOF
-#!/bin/bash
-set -x
-
-umount /dev/xvdb
-mkfs.ext4 /dev/xvdb
-sed -i.bak '/xvdb/d' /etc/fstab
-echo -e '/dev/xvdb\t/var/lib/gravity\text4\tdefaults\t0\t2' >> /etc/fstab
-mkdir -p /var/lib/gravity
-mount /var/lib/gravity
-chown -R 1000:1000 /var/lib/gravity
-sed -i.bak 's/Defaults    requiretty/#Defaults    requiretty/g' /etc/sudoers
-export SUDO_USER=centos
-export SUDO_UID=1000
-export SUDO_GID=1000
-
-# This calls opscenter to start the provision k8s job
-curl --tlsv1.2 --insecure '${var.ops_url}/${var.ops_token}/node?provisioner=aws_terraform&bg=true' | bash
-
-EOF
 
   root_block_device {
     delete_on_termination = true
