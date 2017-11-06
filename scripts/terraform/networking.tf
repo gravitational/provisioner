02b87edf1162826a3399182ea8926b0fc6322f78
Helgi Þorbjörnsson <helgith@gmail.com>
2017-11-06 14:41:31 -0800

    Use locals instead of variables for interpolations

--- scripts/terraform/networking.tf
+++ scripts/terraform/networking.tf
@@ -1,7 +1,7 @@
 // private subnets and routing tables
 resource "aws_route_table" "private" {
   count  = "${length(var.azs)}"
-  vpc_id = "${var.vpc_id}"
+  vpc_id = "${local.vpc_id}"
 
   tags = "${merge(local.common_tags, map(
     "Name", "${var.cluster_name}-private"
@@ -12,13 +12,13 @@ resource "aws_route" "private_nat" {
   count                  = "${length(var.azs)}"
   route_table_id         = "${element(aws_route_table.private.*.id, count.index)}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${element(var.nat_gateways, count.index)}"
+  nat_gateway_id         = "${element(local.nat_gateways, count.index)}"
   depends_on             = ["aws_route_table.private"]
 }
 
 resource "aws_subnet" "private" {
   count             = "${length(var.azs)}"
-  vpc_id            = "${var.vpc_id}"
+  vpc_id            = "${local.vpc_id}"
   cidr_block        = "${element(var.subnets, count.index)}"
   availability_zone = "${element(var.azs, count.index)}"
 
@@ -37,7 +37,7 @@ resource "aws_route_table_association" "private" {
 // public subnets and routing tables
 resource "aws_route_table" "public" {
   count  = "${length(var.azs)}"
-  vpc_id = "${var.vpc_id}"
+  vpc_id = "${local.vpc_id}"
 
   tags = "${merge(local.common_tags, map(
     "Name", "${var.cluster_name}-public"
@@ -48,13 +48,13 @@ resource "aws_route" "public_gateway" {
   count                  = "${length(var.azs)}"
   route_table_id         = "${element(aws_route_table.public.*.id, count.index)}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${var.internet_gateway_id}"
+  gateway_id             = "${local.internet_gateway_id}"
   depends_on             = ["aws_route_table.public"]
 }
 
 resource "aws_subnet" "public" {
   count             = "${length(var.azs)}"
-  vpc_id            = "${var.vpc_id}"
+  vpc_id            = "${local.vpc_id}"
   cidr_block        = "${element(var.public_subnets, count.index)}"
   availability_zone = "${element(var.azs, count.index)}"
 
@@ -73,7 +73,7 @@ resource "aws_route_table_association" "public" {
 // security groups
 resource "aws_security_group" "kubernetes" {
   name   = "${var.cluster_name}"
-  vpc_id = "${var.vpc_id}"
+  vpc_id = "${local.vpc_id}"
   tags   = "${merge(local.common_tags, map())}"
 }
 
