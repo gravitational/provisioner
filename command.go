a244bd9c777817f191c162e39404dec4c7bb9910
Kevin Nisbet <kevin@gravitational.com>
2017-12-05 21:17:25 -0500

    enable debug logging

--- command.go
+++ command.go
@@ -159,6 +159,9 @@ func (c *Command) Run(args []string) error {
 	var err error
 	invokedCommad, err := c.App.Parse(args)
 
+	// temporarily set the log level to debug
+	log.SetLevel(log.DebugLevel)
+
 	if err != nil {
 		return trace.Wrap(err)
 	}
