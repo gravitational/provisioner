623659001cbc211cbe723cea27bd802c423c83c6
Helgi Þormar Þorbjörnsson <70530+helgi@users.noreply.github.com>
2018-06-15 14:10:38 -0700

    Bump to 1.23
    https://github.com/terraform-providers/terraform-provider-aws/blob/master/CHANGELOG.md#1230-june-14-2018

--- scripts/terraform/base.tf
+++ scripts/terraform/base.tf
@@ -16,7 +16,7 @@ variable "aws_max_retries" {
 }
 
 provider "aws" {
-  version                 = "~> 1.22"
+  version                 = "~> 1.23"
   region                  = "${var.aws_region}"
   shared_credentials_file = "/var/lib/telekube/aws-credentials"
   profile                 = "default"
