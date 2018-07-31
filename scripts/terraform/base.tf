f5d468a6396144899f6b23def393bd53e835dd53
Helgi Þormar Þorbjörnsson <70530+helgi@users.noreply.github.com>
2018-07-30 19:17:35 -0700

    Update TF aws provider to 1.29 (#56)
    * Update TF aws provider to 1.27
    
    https://github.com/terraform-providers/terraform-provider-aws/blob/master/CHANGELOG.md#1270-july-11-2018
    
    * Bump to 1.29
    
    https://github.com/terraform-providers/terraform-provider-aws/blob/master/CHANGELOG.md#1290-july-26-2018

--- scripts/terraform/base.tf
+++ scripts/terraform/base.tf
@@ -16,7 +16,7 @@ variable "aws_max_retries" {
 }
 
 provider "aws" {
-  version                 = "~> 1.25"
+  version                 = "~> 1.29"
   region                  = "${var.aws_region}"
   shared_credentials_file = "/var/lib/telekube/aws-credentials"
   profile                 = "default"
